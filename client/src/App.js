f8d38c6c5fa70b6ca538515aadf85b9e8d34dd8f
oelglad <omnia_elglad@aol.com>
2020-01-17 09:29:19 -0500

    Updatede components and services folders and files

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,10 @@ import {Route, Link } from 'react-router-dom';
 import axios from 'axios';
 import './App.css';
 import { loginUser, registerUser, verifyUser, restrict } from './services/api_helper';
-import LoginForm from './components/LoginForm';
-import RegisterForm from './components/RegisterForm';
-import AllPosts from './components/CreateJob';
-import CreatePost from './components/Jobs';
+import LoginForm from './components/loginForm';
+import RegisterForm from './components/registerForm';
+import AllPosts from './components/createJob';
+import CreatePost from './components/jobs';
 
 class App extends Component {
   constructor(props) {
