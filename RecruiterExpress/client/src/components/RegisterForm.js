d11387eebd352c994bbb57f951286bc2caa10f85
Henry <hcewurum@gmail.com>
2020-01-17 10:06:22 -0500

    changed file structure andname to carmel case

--- RecruiterExpress/client/src/components/RegisterForm.js
+++ RecruiterExpress/client/src/components/RegisterForm.js
@@ -1,1 +1,11 @@
-import React from 'react';
+import React from 'react';import React from 'react';
+
+
+function RegisterForm() {
+  
+  return (
+    <div></div>
+  )
+} 
+
+export default RegisterForm
