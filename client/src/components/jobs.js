cf56e42f4c31ff429222102edfd294a386c04a96
Joseph Pinangay <pinangayjoel@gmail.com>
2020-01-17 13:20:02 -0500

    [home.js][jobs.js][registerForm.js] is updated

--- client/src/components/jobs.js
+++ client/src/components/jobs.js
@@ -1,1 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function AnimalList(props) {
+  return (
+    <div>
+      {props.namethis.map(name => (
+        <div>{name}</div>
+      ))}
+    </div>
+  )
+}
