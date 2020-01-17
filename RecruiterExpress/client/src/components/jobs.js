a52a41a83639bd988f1ac464b401c4378829f0de
Joseph Pinangay <pinangayjoel@gmail.com>
2020-01-17 14:50:29 -0500

    [jobs.js] is able to display all the jobs

--- RecruiterExpress/client/src/components/jobs.js
+++ RecruiterExpress/client/src/components/jobs.js
@@ -1,12 +1,42 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-export default function Jobs(props) {
-  return (
-    <div>
-      {props.namethis.map(name => (
-        <div>{name}</div>
-      ))}
-    </div>
-  )
+export default class Jobs extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      jobs: []
+    }
+  }
+
+
+  async componentDidMount() {
+    try {
+      const response = await axios.get('http://localhost:3001/jobs');
+      console.log(response);
+      this.setState({
+        jobs: response.data
+      })
+    } catch (e) {
+      console.error(e);
+    }
+    console.log(this.state)
+  }
+
+
+  render() {
+    return (
+      <div>
+        {this.state.jobs.map(jobs => (
+          <div>
+            <h3>{jobs.jobTitle}</h3>
+            <h4>{jobs.jobId}</h4>
+            <p>{jobs.jobDescription}</p>
+            <p>{jobs.jobRequirements}</p>
+          </div>
+        ))}
+      </div>
+    )
+  }
 }
