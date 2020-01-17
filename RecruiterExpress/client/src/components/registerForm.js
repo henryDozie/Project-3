f5ecb3d7130689e35edbd9cab2fecc8123585d46
Joseph Pinangay <pinangayjoel@gmail.com>
2020-01-17 13:20:02 -0500

    [home.js][jobs.js][registerForm.js] is updated

--- RecruiterExpress/client/src/components/registerForm.js
+++ RecruiterExpress/client/src/components/registerForm.js
@@ -1,1 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
+
+export default class RegisterForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: '',
+      password: ''
+    }
+  }
+
+  handleChange = (e) => {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value
+    })
+  }
+
+
+
+  render() {
+    return (
+      <form onSubmit={(e) => this.props.handleRegister(e, { username: this.state.username, password: this.state.password })}>
+        <h2>Register!!!</h2>
+        <label htmlFor="username">Username</label>
+        <input
+          type='text'
+          name='username'
+          value={this.state.username}
+          onChange={this.handleChange} />
+
+        <label htmlFor="password">Password</label>
+        <input
+          type='password'
+          name='password'
+          value={this.state.password}
+          onChange={this.handleChange} />
+        <input type='submit' />
+      </form>
+    )
+  }
+}
