5107a7ea2c2fea932ea92634fcd1752aab21a9d8
oelglad <omnia_elglad@aol.com>
2020-01-17 12:21:02 -0500

    [footer.js]

---
+++ client/src/components/footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+const Footer = () => (
+    <div class="Footer">
+        <p>WingRecruiter, Inc. © All Rights Reserved Worldwide</p>
+    </div>
+
+)
+
+export default Footer;
