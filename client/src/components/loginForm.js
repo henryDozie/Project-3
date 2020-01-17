39c5c6c56ad2b8e5b3a6facc6707003a3d372725
Henry <hcewurum@gmail.com>
2020-01-17 17:14:31 -0500

    cleaned up some of the login components

--- client/src/components/loginForm.js
+++ client/src/components/loginForm.js
@@ -1,44 +1,56 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+// import axios from "axios";
 
-export default class LoginForm extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            username: "",
-            password: ""
+class LoginForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: ""
+    };
+  }
+
+  onChange = async e => {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  render() {
+    return (
+      <form
+        className="login"
+        onSubmit={e =>
+          this.props.handleLogin(e, {
+            username: this.state.username,
+            password: this.state.password
+          })
         }
-    }
-    handleChange = (e) => {
-        const { name, value } = e.target
-        this.setState({
-            [name]: value
-        })
-    }
-
-    render() {
-        return (
-            <div class="container">
-                <form class="row" onSubmit={(e) => this.props.handleLogin(e, { username: this.state.username, password: this.state.password })}>
-
-                    <label htmlFor="username">Username</label>
-                    <input
-                        type="text"
-                        name="username"
-                        value={this.state.username}
-                        onChange={this.handleChange}
-                    />
-                    <label htmlFor="password">Password</label>
-                    <input
-                        type="text"
-                        name="password"
-                        value={this.state.password}
-                        onChange={this.handleChange}
-                    />
-                    <input type="submit" />
-                    {/* <Link to = "/register">Register</Link> */}
-                </form>
-            </div>
-        )
-    }
+      >
+        <h2>Login</h2>
+        <label htmlFor="username">Username</label>
+        <input
+          type="text"
+          name="username"
+          value={this.state.username}
+          onChange={this.onChange}
+          required
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          name="password"
+          value={this.state.password}
+          onChange={this.onChange}
+          required
+        />
+        <input className="submit" type="submit" value="Login" />
+        <Link to="/register">Register</Link>
+      </form>
+    );
+  }
 }
+
+export default LoginForm;
