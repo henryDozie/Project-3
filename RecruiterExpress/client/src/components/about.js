f25a7cb81318b37d3ac44e3426c999f2aee60376
oelglad <omnia_elglad@aol.com>
2020-01-19 12:23:34 -0500

    fixing merging issue

---
+++ RecruiterExpress/client/src/components/about.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const About =()=>{
+return (
+    <div className="AboutClass">
+        Synchronized with the notion "Simplicity is the ultimater sophostication"; Stocknomics is 
+        a simple yet sophisticated website designed to provide stock quotes to users .Users are
+        able to obtain daily stock quotes and other details around thier favorite stocks. 
+    </div>
+
+)
+}
+export default About;
--- RecruiterExpress/client/src/components/registerForm.js
+++ RecruiterExpress/client/src/components/registerForm.js
@@ -1,53 +1,53 @@
-  import React, { Component } from "react";
+import React, { Component } from "react";
 
-  class RegisterForm extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        username: "",
-        password: ""
-      };
-    }
-  
-    handleChange = e => {
-      const { name, value } = e.target;
-      this.setState({
-        [name]: value
-      });
+class RegisterForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: ""
     };
-  
-    render() {
-      return (
-        <form
-          className="register"
-          onSubmit={e =>
-            this.props.handleRegister(e, {
-              username: this.state.username,
-              password: this.state.password
-            })
-          }
-        >
-          <h2>Register</h2>
-          <label htmlFor="username">Username</label>
-          <input
-            type="text"
-            name="username"
-            value={this.state.username}
-            onChange={this.handleChange}
-            required
-          />
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            name="password"
-            value={this.state.password}
-            onChange={this.handleChange}
-            required
-          />
-          <input className="submit" type="submit" value="Create Account" />
-        </form>
-      );
-    }
   }
-  
-  export default RegisterForm;
+
+  handleChange = e => {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  render() {
+    return (
+      <form
+        className="register"
+        onSubmit={e =>
+          this.props.handleRegister(e, {
+            username: this.state.username,
+            password: this.state.password
+          })
+        }
+      >
+        <h2>Register</h2>
+        <label htmlFor="username">Username</label>
+        <input
+          type="text"
+          name="username"
+          value={this.state.username}
+          onChange={this.handleChange}
+          required
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          name="password"
+          value={this.state.password}
+          onChange={this.handleChange}
+          required
+        />
+        <input className="submit" type="submit" value="Create Account" />
+      </form>
+    );
+  }
+}
+
+export default RegisterForm;
