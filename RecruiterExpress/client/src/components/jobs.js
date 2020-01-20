ff0a33f1649d106a8ec0a5f1ac83ff75a86b3e1a
Joseph Pinangay <pinangayjoel@gmail.com>
2020-01-20 00:09:34 -0500

    modified jobs.js so that it receives a this.props.jobs from Home.js

--- RecruiterExpress/client/src/components/jobs.js
+++ RecruiterExpress/client/src/components/jobs.js
@@ -6,20 +6,8 @@ export default class Jobs extends Component {
   constructor(props) {
     super(props);
 
-
   }
-  // async componentDidMount() {
-  //   try {
-  //     const response = await axios.get('http://localhost:3001/jobs');
-  //     console.log(response);
-  //     this.setState({
-  //       jobs: response.data
-  //     })
-  //   } catch (e) {
-  //     console.error(e);
-  //   }
-  //   console.log(this.state)
-  // }
+
 
 
   render() {
