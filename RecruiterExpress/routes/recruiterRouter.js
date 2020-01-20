23e6e483ad15ed382bccb15aedfbd30cc0eb345b
Henry <hcewurum@gmail.com>
2020-01-20 00:06:23 -0500

    [recruiterRouter.js] modified the routing options for the recruiter

--- RecruiterExpress/routes/recruiterRouter.js
+++ RecruiterExpress/routes/recruiterRouter.js
@@ -1,6 +1,63 @@
-const { Router } = require('express');
+const { Router } = require("express");
 const recruiterRouter = Router();
-const { Recruiter } = require('../models.js')
+const { Recruiter } = require("../models.js");
+
+// recruiterRouter.get("/", async (req, res) => {
+//   try {
+//     const recruiters = await Recruiter.findAll();
+//     res.json(recruiters);
+//   } catch (e) {
+//     next(e);
+//   }
+// });
+
+// //show
+// recruiterRouter.get("/:id", async (req, res) => {
+//   const id = req.params.id;
+//   try {
+//     const recruiters = await Recruiter.findByPk(id);
+//     res.json({ recruiters });
+//   } catch (e) {
+//     next(e);
+//   }
+// });
+
+// recruiterRouter.post("/", async (req, res) => {
+//   const data = req.body;
+//   try {
+//     const recruiters = await Recruiter.create(data);
+//     res.json({ recruiters });
+//   } catch (e) {
+//     next(e);
+//   }
+// });
+
+// //update
+// recruiterRouter.put("/:id", async (req, res) => {
+//   const id = req.params.id;
+//   const data = req.body;
+//   try {
+//     const recruiters = await Recruiter.findByPk(id);
+//     await recruiters.update(data);
+//     res.json({ recruiters });
+//   } catch (e) {
+//     next(e);
+//   }
+// });
+
+// //delete
+// recruiterRouter.delete("/:id", async (req, res) => {
+//   const id = req.params.id;
+//   try {
+//     const recruiters = await Recruiter.findByPk(id);
+//     await speaker.destroy();
+//     res.json({ recruiters });
+//   } catch (e) {
+//     next(e);
+//   }
+// });
+
+// module.exports = recruiterRouter;
 
 recruiterRouter.route('/')
   .get(async (req, res, next) => {
