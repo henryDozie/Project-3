a4e1a97e24daeab4920da47f7ac43c9b1a8b66a3
henrydozie <hcewurum@gmail.com>
2020-01-26 09:58:22 -0500

    Added the heroku base URL

--- RecruiterExpress/client/src/services/api_helper.js
+++ RecruiterExpress/client/src/services/api_helper.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "http://localhost:3001"
+  // baseURL: "https://hidden-fortress-18345.herokuapp.com/"
 });
 
 export const loginUser = async loginData => {
